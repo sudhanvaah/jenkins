e1f45330c4202f6e4a25bcbbc12f05cf6af2b31a
Sudhanva <sudhanvaah@users.noreply.github.com>
2017-02-14 10:28:44 +0530

    Update TestBankAccount.java

--- lab5/src/test/java/ca/uwo/csd/cs2212/sudhegde/TestBankAccount.java
+++ lab5/src/test/java/ca/uwo/csd/cs2212/sudhegde/TestBankAccount.java
@@ -5,7 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class TestBankAccount {
-  BankAccount account = BankAccount(10);
+  BankAccount account = new BankAccount(10);
   double amount = account.debit(5);
   Assert.assertEquals(5.0,amount);
 }
